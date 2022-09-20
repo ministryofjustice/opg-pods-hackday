f8b7140c17d529487696494dd363ae6c250e795a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-09-20 15:00:26 +0100

    set up accunt configuration

---
+++ service-add-lpa-to-pod/terraform/account/terraform.tf
@@ -0,0 +1,82 @@
+terraform {
+  backend "s3" {
+    bucket         = "opg.terraform.state"
+    key            = "pods-hackday-account/terraform.tfstate"
+    encrypt        = true
+    region         = "eu-west-1"
+    role_arn       = "arn:aws:iam::311462405659:role/pods-hackday-ci"
+    dynamodb_table = "remote_lock"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.31.0"
+    }
+  }
+  required_version = ">= 1.2.6"
+}
+
+variable "default_role" {
+  type    = string
+  default = "modernising-lpa-ci"
+}
+
+provider "aws" {
+  alias  = "eu_west_1"
+  region = "eu-west-1"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::995199299616:role/${var.default_role}"
+    session_name = "pods-hackday-terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "management_eu_west_1"
+  region = "eu-west-1"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    session_name = "pods-hackday-terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "global"
+  region = "us-east-1"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::995199299616:role/${var.default_role}"
+    session_name = "pods-hackday-terraform-session"
+  }
+}
+
+data "aws_region" "eu_west_1" {
+  provider = aws.eu_west_1
+}
+
+data "aws_caller_identity" "eu_west_1" {
+  provider = aws.eu_west_1
+}
+
+data "aws_default_tags" "eu_west_1" {
+  provider = aws.eu_west_1
+}
+
+data "aws_region" "global" {
+  provider = aws.global
+}
+
+data "aws_caller_identity" "global" {
+  provider = aws.global
+}
+
+data "aws_default_tags" "global" {
+  provider = aws.global
+}
---
+++ service-add-lpa-to-pod/terraform/account/variables.tf
@@ -0,0 +1,37 @@
+output "workspace_name" {
+  value = terraform.workspace
+}
+
+variable "accounts" {
+  type = map(
+    object({
+      account_id    = string
+      account_name  = string
+      is_production = bool
+      regions       = list(string)
+    })
+  )
+}
+
+locals {
+  account_name = lower(replace(terraform.workspace, "_", "-"))
+  account      = var.accounts[local.account_name]
+
+  mandatory_moj_tags = {
+    business-unit    = "OPG"
+    application      = "opg-pods-hackday-add-lpa-to-pod"
+    environment-name = local.account.account_name
+    owner            = "OPG Webops: opgteam+pods-hackday@digital.justice.gov.uk"
+    is-production    = local.account.is_production
+    runbook          = "https://github.com/ministryofjustice/opg-pods-hackday"
+    source-code      = "https://github.com/ministryofjustice/opg-pods-hackday"
+  }
+
+
+  optional_tags = {
+    account-name           = local.account.account_name
+    infrastructure-support = "OPG Webops: opgteam+pods-hackday@digital.justice.gov.uk"
+  }
+
+  default_tags = merge(local.mandatory_moj_tags, local.optional_tags)
+}
